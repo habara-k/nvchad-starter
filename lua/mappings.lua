09b32a287aad531bdb78cf40fab9aeb989f5e8a9
Keigo Habara <k.habara.aa@gmail.com>
2024-04-11 22:28:01 +0900

    Remove <C-Space> keymap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,9 +6,9 @@ local map = vim.keymap.set
 
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map("i", "jk", "<ESC>")
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
+map("n", "<C-Space>", "<Nop>")
 map("n", "<C-w>", "<C-w>q")
 map("n", "<leader>tt", function()
   require("base46").toggle_transparency()
